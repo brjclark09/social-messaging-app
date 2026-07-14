1e3f5974428732077f21db4345a1efeee802cecd
Brandon Clark <brjclark09@gmail.com>
2026-07-14 05:55:43 -0500

    fixing API connection

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'https://localhost:8888';
+const BASE_URL = 'http://192.168.1.200:5000';
 
 const api = axios.create({
   baseURL: BASE_URL,
